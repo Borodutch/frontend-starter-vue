5c4ba83b56b9db5716faf94c2637c1717a0f7287
upacyxou <upacyxou@tutanota.com>
2020-10-20 21:35:59 +0400

    fix imports

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,40 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, {
+  VApp,
+  VAppBar,
+  VToolbarTitle,
+  VSpacer,
+  VBtn,
+  VIcon,
+  VMenu,
+  VList,
+  VListItem,
+  VListItemTitle,
+  VFlex,
+  VLayout,
+  VSnackbar,
+  VMain,
+} from 'vuetify/lib'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VAppBar,
+    VToolbarTitle,
+    VSpacer,
+    VBtn,
+    VIcon,
+    VMenu,
+    VList,
+    VListItem,
+    VListItemTitle,
+    VFlex,
+    VLayout,
+    VSnackbar,
+    VMain,
+  },
+})
 
 export default new Vuetify({
   icons: {
